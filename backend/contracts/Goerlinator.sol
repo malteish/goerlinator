ab8a32209eb089dfcefa0a1b733a2f5ebf66435b
malteish <malteish@corpus.io>
2023-03-03 09:31:45 -0700

    add basic tests

--- backend/contracts/Goerlinator.sol
+++ backend/contracts/Goerlinator.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.19;
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -7,24 +7,38 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Goerlinator is Pausable, ReentrancyGuard, Ownable {
     mapping(address => bool) public eligibleAddresses;
 
+    uint256 public claimAmount = 1 ether;
+
     event Claimed(address indexed user, uint256 amount);
+    event ClaimAmountUpdated(uint256 claimAmount);
 
     /**
      * @dev Initializes the contract, setting the owner.
      */
-    constructor() Ownable() {}
+    constructor(uint256 _claimAmount) Ownable() {
+        claimAmount = _claimAmount;
+        emit ClaimAmountUpdated(_claimAmount);
+    }
 
     /**
      * @dev Claim goerli eth if you are eligible. Eligibility is nullified after claiming.
      */
-    function claim() public nonReentrant whenNotPaused {
+    function claim() public whenNotPaused {
+        claimFor(msg.sender);
+    }
+
+    /**
+     * @dev Claim goerli eth for someone else if that address is eligible.
+     * Eligibility is nullified after claiming.
+     */
+    function claimFor(address _address) public nonReentrant whenNotPaused {
         require(
-            eligibleAddresses[msg.sender] == true,
-            "You are not eligible to claim"
+            eligibleAddresses[_address] == true,
+            "Address not eligible to claim"
         );
-        eligibleAddresses[msg.sender] = false;
-        payable(msg.sender).transfer(1 ether);
-        emit Claimed(msg.sender, 1 ether);
+        eligibleAddresses[_address] = false;
+        payable(_address).transfer(1 ether);
+        emit Claimed(_address, 1 ether);
     }
 
     /**
@@ -39,9 +53,24 @@ contract Goerlinator is Pausable, ReentrancyGuard, Ownable {
     }
 
     /**
+     * @dev Set the claim amount.
+     */
+    function setClaimAmount(uint _claimAmount) public onlyOwner {
+        claimAmount = _claimAmount;
+        emit ClaimAmountUpdated(_claimAmount);
+    }
+
+    /**
      * @dev Withdraws all the funds from the contract.
      */
     function withdraw() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
+
+    /**
+     * @dev enables the contract to receive funds.
+     */
+    receive() external payable {}
+
+    fallback() external payable {}
 }
---
+++ backend/test/Goerlinator.t.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+import "../lib/forge-std/src/Test.sol";
+import "../contracts/Goerlinator.sol";
+
+contract GoerlinatorTest is Test {
+    Goerlinator goerlinator;
+
+    uint256 claimAmount = 1 ether;
+
+    address owner = address(3);
+    address eligibleAddress1 = address(10);
+    address eligibleAddress2 = address(11);
+    address notEligibleAddress = address(20);
+
+    function setUp() public {
+        goerlinator = new Goerlinator(claimAmount);
+        goerlinator.transferOwnership(owner);
+        vm.deal(owner, 834 ether);
+        vm.prank(owner);
+        payable(address(goerlinator)).transfer(200 ether);
+        address[] memory eligibleAddresses = new address[](2);
+        eligibleAddresses[0] = eligibleAddress1;
+        eligibleAddresses[1] = eligibleAddress2;
+        vm.prank(owner);
+        goerlinator.makeAddressesEligible(eligibleAddresses);
+    }
+
+    function testOwnerCanWithdraw() public {
+        assertTrue(goerlinator.owner() == owner);
+        uint balanceBefore = address(owner).balance;
+        uint contractBalanceBefore = address(goerlinator).balance;
+        vm.prank(owner);
+        goerlinator.withdraw();
+        uint balanceAfter = address(owner).balance;
+        assertEq(
+            balanceAfter,
+            balanceBefore + contractBalanceBefore,
+            "Owner balance should increase by contract balance"
+        );
+        assertEq(
+            address(goerlinator).balance,
+            0,
+            "Contract balance should be 0"
+        );
+    }
+
+    function testNonOwnersCanNotWithdraw(address x) public {
+        vm.assume(x != owner);
+        vm.assume(x.balance == 0);
+        uint balanceBefore = address(goerlinator).balance;
+
+        vm.prank(address(x));
+        vm.expectRevert("Ownable: caller is not the owner");
+        goerlinator.withdraw();
+        assertEq(address(x).balance, 0, "Address balance should be 0");
+        assertEq(
+            address(goerlinator).balance,
+            balanceBefore,
+            "Contract balance should not change"
+        );
+    }
+
+    function testClaimWhenEligible() public {
+        uint balanceBefore = address(eligibleAddress1).balance;
+        assertEq(
+            goerlinator.eligibleAddresses(eligibleAddress1),
+            true,
+            "Address should be eligible"
+        );
+        vm.prank(address(eligibleAddress1));
+        goerlinator.claim();
+        assertEq(
+            address(eligibleAddress1).balance,
+            balanceBefore + claimAmount,
+            "Address balance should increase by claim amount"
+        );
+        assertEq(
+            goerlinator.eligibleAddresses(eligibleAddress1),
+            false,
+            "Address should not be eligible after claiming"
+        );
+    }
+
+    function testClaimForEligibleAddress(address x) public {
+        vm.assume(goerlinator.eligibleAddresses(x) == false);
+        uint balanceBefore = address(eligibleAddress1).balance;
+        assertEq(
+            goerlinator.eligibleAddresses(eligibleAddress1),
+            true,
+            "Address should be eligible"
+        );
+        vm.prank(x);
+        goerlinator.claimFor(eligibleAddress1);
+        assertEq(
+            address(eligibleAddress1).balance,
+            balanceBefore + claimAmount,
+            "Address balance should increase by claim amount"
+        );
+        assertEq(
+            goerlinator.eligibleAddresses(eligibleAddress1),
+            false,
+            "Address should not be eligible after claiming"
+        );
+    }
+
+    // function testFailNotOwnerRemove(address x) public {
+    //     vm.assume(x != owner);
+    //     vm.prank(address(x));
+    //     vm.expectRevert();
+    //     goerlinator.withdraw(address(0));
+    // }
+
+    // function testSet() public {
+    //     assertTrue(goerlinator.map(address(0)) == 0);
+    //     goerlinator.set(address(0), 1);
+    //     assertTrue(goerlinator.map(address(0)) == 1);
+    // }
+
+    // function testSet2() public {
+    //     assertTrue(goerlinator.map(address(1)) == 0);
+    //     goerlinator.set(address(1), 2);
+    //     assertTrue(goerlinator.map(address(1)) == 2);
+    // }
+
+    // function testSetFuzzingAddress(address x) public {
+    //     assertTrue(goerlinator.map(address(x)) == 0);
+    //     goerlinator.set(address(x), 1);
+    //     assertTrue(goerlinator.map(address(x)) == 1);
+    // }
+
+    // function testSetFuzzingValue(uint256 x) public {
+    //     assertTrue(goerlinator.map(address(0)) == 0);
+    //     goerlinator.set(address(0), x);
+    //     assertTrue(goerlinator.map(address(0)) == x);
+    // }
+
+    // function testRemove(address x) public {
+    //     if (x == address(0)) return;
+    //     assertTrue(goerlinator.map(address(0)) == 0);
+    //     goerlinator.set(address(0), 1);
+    //     assertTrue(goerlinator.map(address(0)) == 1);
+
+    //     assertTrue(goerlinator.map(x) == 0);
+    //     goerlinator.set(x, 1);
+    //     assertTrue(goerlinator.map(x) == 1);
+
+    //     goerlinator.remove(address(0));
+    //     assertTrue(goerlinator.map(address(0)) == 0);
+    //     assertTrue(goerlinator.map(x) == 1);
+
+    //     goerlinator.remove(x);
+    //     assertTrue(goerlinator.map(address(0)) == 0);
+    //     assertTrue(goerlinator.map(x) == 0);
+    // }
+}
